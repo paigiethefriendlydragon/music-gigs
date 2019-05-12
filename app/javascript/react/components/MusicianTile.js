c5374c38bdfcec095cb2a06f867f6326fc3eb287
paigiethefriendlydragon <paigekiefner@gmail.com>
2019-05-12 13:06:00 -0400

    Sign in page updated

--- app/javascript/react/components/MusicianTile.js
+++ app/javascript/react/components/MusicianTile.js
@@ -4,7 +4,7 @@ const MusicianTile = (props) => {
   return(
     <div>
         <div id="name">
-            <h2>{props.first_name} {props.last_name} </h2>
+          <h2>{props.first_name} {props.last_name} </h2>
         </div>
         <div id="description">
           <p>{props.description}</p>
