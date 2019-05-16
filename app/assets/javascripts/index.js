c2ed6856c0172d353a7da2e221c0bad5bd87926b
paigiethefriendlydragon <paigekiefner@gmail.com>
2019-05-16 09:51:16 -0400

    Working but verbose dropdown menu

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -12,3 +12,29 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 4000); // Change image every 2 seconds
 }
+
+
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+// function myFunction() {
+//
+//   document.getElementById("myDropdown").classList.toggle("show");
+// }
+// function nestFunction() {
+//   document.getElementById("myDropdownNest").classList.toggle("show");
+// }
+
+// Close the dropdown menu if the user clicks outside of it
+// window.onclick = function(event) {
+//   if (!event.target.matches('.dropbtn')){
+//     var dropdowns = document.getElementsByClassName("dropdown-content");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// }
--- app/javascript/react/components/MusicianTile.js
+++ app/javascript/react/components/MusicianTile.js
@@ -2,28 +2,39 @@ import React, { Component } from 'react'
 
 const MusicianTile = (props) => {
   return(
-    <div>
+      <div>
         <div id="pic" className="">
           <img src= {props.pic}/>
+          </div>
+          <div class="row">
+            <div class="column">
+              <div id="name" class="name-container">
+                  <p>{props.first_name} {props.last_name} </p>
+          </div>
+          <div id="description" class="description-container">
+            <p>{props.description}</p>
         </div>
-        <div id="name" className="name-container">
-          <p>{props.first_name} {props.last_name} </p>
+
+
+        <div class="column">
+          <div id="rate" className="rate-container">
+            <p>Hourly rate: {props.rate}</p>
+          </div>
+
+          <div id="gigRate" className="rate-container">
+            <p>Rate per gig: {props.gigRate}</p>
+          </div>
+
+          <div id="youtube" className="link-container">
+            <p>Check out our youtube: {props.youtube}</p>
+          </div>
+
+          <div id="soundcloud" className="link-container">
+            <p> Check out our soundcloud: {props.soundcloud}</p>
+          </div>
         </div>
-        <div id="description" className="description-container">
-          <p>{props.description}</p>
-        </div>
-        <div id="rate" className="rate-container">
-          <p>{props.rate}</p>
-        </div>
-        <div id="gigRate" className="rate-container">
-          <p>{props.gigRate}</p>
-        </div>
-        <div id="youtube" className="link-container">
-          <p>{props.youtube}</p>
-        </div>
-        <div id="soundcloud" className="link-container">
-          <p>{props.soundcloud}</p>
-        </div>
+      </div>
+    </div>
     </div>
   )
 }
