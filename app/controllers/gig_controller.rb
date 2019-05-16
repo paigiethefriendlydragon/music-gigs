1d07650a739757a1514945bfe47737bfb9f03bf0
paigiethefriendlydragon <paigekiefner@gmail.com>
2019-05-16 16:03:39 -0400

    Revised menu and styling

--- app/controllers/gig_controller.rb
+++ app/controllers/gig_controller.rb
@@ -3,70 +3,92 @@ class GigController < ApplicationController
   def index
     @users = User.all
     @user_sample = @users.sample(1)
+    @instrument_list = {brass:
+                          ["Trumpet",
+                          "Cornet",
+                          "Piccolo Trumpet",
+                          "Flugelhorn",
+                          "French horn",
+                          "Baritone horn",
+                          "Euphonium",
+                          "Alto horn",
+                          "Tenor horn",
+                          "Trombone",
+                          "Tuba",
+                          "Bugle"
+                        ],
 
-    @brass_menu = ["Trumpet",
-                      "Cornet",
-                      "Piccolo Trumpet",
-                      "Flugelhorn",
-                      "French horn",
-                      "Baritone horn",
-                      "Euphonium",
-                      "Alto horn",
-                      "Tenor horn",
-                      "Trombone",
-                      "Tuba",
-                      "Bugle"]
 
+                          keyboard:
+                            ["Accordion",
+                            "Celesta",
+                            "Clavichord",
+                            "Dulcitone",
+                            "Electronic keyboard",
+                            "harpsichord",
+                            "Organ",
+                            "Piano",
+                            "Synthesizer"
+                          ],
 
-      @keyboard_menu = ["Accordion",
-                        "Celesta",
-                        "Clavichord",
-                        "Dulcitone",
-                        "Electronic keyboard",
-                        "harpsichord",
-                        "Organ",
-                        "Piano",
-                        "Synthesizer"]
+                          strings:
+                            ["violin",
+                            "viola",
+                            "cello",
+                            "string bass",
+                            "harp"
+                          ],
 
-      @orchestral_strings_menu = ["violin",
-                                  "viola",
-                                  "cello",
-                                  "string bass",
-                                  "harp"]
+                          guitar:
+                            ["acoustic guitar",
+                            "banjo",
+                            "bass guitar",
+                            "electric guitar",
+                            "mandolin",
+                            "ukulele"
+                          ],
 
-      @guitar_family_menu = ["acoustic guitar",
-                              "banjo",
-                              "bass guitar",
-                              "electric guitar",
-                              "mandolin",
-                              "ukulele"]
+                          woodwinds:
+                            ["alto flute",
+                            "oboe",
+                            "bass oboe",
+                            "bassoon",
+                            "clarinet",
+                            "contrabass oboe",
+                            "flute",
+                            "piccolo",
+                            "recorder",
+                            "saxophone"
+                          ],
 
-      @woodwinds_menu = ["alto flute",
-                          "oboe",
-                          "bass oboe",
-                          "bassoon",
-                          "clarinet",
-                          "contrabass oboe",
-                          "flute",
-                          "piccolo",
-                          "recorder",
-                          "saxophone"
+                          percussion: [
+                            "ashiko",
+                            "bass drum",
+                            "bongo drum",
+                            "castanet",
+                            "conga",
+                            "cymbal",
+                            "djembe",
+                            "drums",
+                            "gong",
+                            "maracas",
+                            "marimba",
+                            "snare drum",
+                            "tambourine",
+                            "timpani",
+                            "glockenspiel"
                           ]
+                        }
 
-      @percussion_menu = ["ashiko",
-                          "bass drum",
-                          "bongo drum",
-                          "castanet",
-                          "conga",
-                          "cymbal",
-                          "djembe",
-                          "drums",
-                          "gong",
-                          "maracas",
-                          "marimba",
-                          "snare drum",
-                          "tambourine",
-                          "timpani",
-                          "glockenspiel"]
+
+        # @genres = [ {rock:
+        #               "alternative",
+        #               "metal",
+        #               "indie rock",
+        #               "punk",
+        #               "hard rock"},
+        #              ]
+
+
         end
       end
